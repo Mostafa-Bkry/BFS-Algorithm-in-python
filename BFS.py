6242f052a5371820003f7b420ab254179cfc0ca4
Mostafa Ala <61254058+Mostafa-Bkry@users.noreply.github.com>
2021-12-29 22:53:21 +0200

    added-some-debugging-and-colors
    added-some-debugging-and-REDcolor-for-the-target-nodes

--- BFS.py
+++ BFS.py
@@ -23,13 +23,14 @@ graph = {
     "I": []
 }
 '''
+
 graph = {
   1 : [2, 3],
   2 : [1, 3, 4, 5],
-  3 : [1, 4, 5, 6],
+  3 : [4, 5, 6],
   4 : [1, 2, 3, 6],
   6 : [1, 4, 5],
-  5 : [3, 4, 6]
+  5 : [4, 6]
 }
 
 
@@ -64,64 +65,55 @@ def BFS(graph, start_node, target_node):
     while not queue.empty():
         print('In Queue: ', list(queue.queue))
         current_node = queue.get()
+        print('current_node: ', current_node)
         if current_node == target_node:
             path_found = True
             break
         
-        print('current_node: ', graph[current_node])
+        print('current_node_neighbours: ', graph[current_node])
 
         for next_node in graph[current_node]:
             #print(next_node)
             if next_node not in visited:
                 queue.put(next_node)
-                parent[next_node] = current_node
-                #print(parent)
                 visited.add(next_node)
+                parent[next_node] = current_node
+                print('parent: ', parent)
                 
+    print('Last in Queue: ', list(queue.queue))            
     # Path reconstruction
     path = []
     if path_found:
+        #add target node
         path.append(target_node)
+        print('path: ', path)
         while parent[target_node] is not 'No Parent':
-            path.append(parent[target_node]) 
+            #add the parent of target node
+            path.append(parent[target_node])
+            '''
+            make the target node the parent to dive in
+            all parents until reach the start node
+            '''
             target_node = parent[target_node]
+            print('target_node: ', target_node)
         path.reverse()
     return path
 
 
-#Now, say we'd like to search for node 6 starting at node 1:
-path = BFS(graph, 1, 5)
+path = BFS(graph, 1, 6)
 #path = BFS(graph, 'A', 'I')
-print(path)
+print('---------------------------------')
+print('The way is: ', path)
 
+colors = []
+for node in G.nodes():
+    if node in [n for n in path]:
+        colors.append('red')
+    else:
+        colors.append('blue')
 
-def traversal(graph, start_node):
-    visited = set()
-    queue = Queue()
-    queue.put(start_node)
-    visited.add(start_node)
+nx.draw(G, with_labels = True, node_size = 700, font_color = 'w'
+        , node_color = colors)
 
-    while not queue.empty():
-        current_node = queue.get()
-        print(current_node, end = " ")
-        for next_node in graph[current_node]:
-            if next_node not in visited:
-                queue.put(next_node)
-                visited.add(next_node)
-                
-#Now, let's define a simple graph:
-'''
-graph = {
-    1 : [2, 3],
-    2 : [1, 3, 5],
-    3 : [1, 2, 4],
-    4 : [3],
-    5 : [2]
-}
-traversal(graph, 1)
-'''
-
-#nx.draw(G, with_labels = True, node_size = 700, font_color = 'w')
-
-#import matplotlib.pyplot as plt
-#plt.show()
+import matplotlib.pyplot as plt
+plt.show()
